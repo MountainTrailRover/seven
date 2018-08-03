3e93ea23ef1cbcba16dd8f816d1f49774d3cd5c7
MountainTrailRover <MountainTrailRover@gmail.com>
2018-08-03 05:15:07 -0700

    Added CSS

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
   constructor(props) {
      super(props);
+
+     this.state = {
+       items: []
+     };
 
      this.addItem = this.addItem.bind(this);
    }
@@ -26,18 +32,19 @@ class TodoList extends Component {
        console.log(this.state.items);
 
        e.preventDefault();
-   }
+     }
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
           <form onSubmit={this.addItem}>
-            <input ref={(a) => this._inputElement = a} 
+            <input ref={(a) => this._inputElement = a}
             placeholder="enter task">
             </input>
             <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items}/>
       </div>
     );
   }
